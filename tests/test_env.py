27b3af6f3a450a53a78b9819414f48e884bc3853
Alejandro-arguelles <alejandro.ar.tj@gmail.com>
2025-11-08 17:35:09 +0100

    set up

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,6 +1,6 @@
-import pytest
+# tests/test_env.py
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # añade la raíz al PYTHONPATH
 
-
-## replace with your project folder name
-from project.utils import *
-from project.models import Model1, Model2
+from utils import *                 # en lugar de mva_tda.utils
+from model import Model1, Model2    # en lugar de mva_tda.models
